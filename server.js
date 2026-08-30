1eb5483483683eacfa6975c690b9e079e965a0e1
Sithma Ravihara <srsithma@gmail.com>
2026-08-30 09:14:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,74 @@
-const express = require("express")
-const fs = require("fs")
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
-app.use(express.json())
+const PORT = process.env.PORT || 3000;
+const codesFile = path.join(__dirname, "codes.json");
 
-app.get("/api/codes",(req,res)=>{
+// Middleware
+app.use(express.json());
 
-let data = fs.readFileSync("codes.json")
-
-res.json(JSON.parse(data))
-
-})
-
-app.post("/api/update",(req,res)=>{
-
-fs.writeFileSync("codes.json",JSON.stringify(req.body,null,2))
-
-res.send("updated")
-
-})
-
-app.listen(3000,()=>{
-console.log("SRVpn Server Running")
-})
-
-
+// Online users counter
 let onlineUsers = 0;
 
-app.get("/api/users", (req,res)=>{
-  res.json({users: onlineUsers});
+app.use((req, res, next) => {
+  onlineUsers++;
+
+  res.on("finish", () => {
+    onlineUsers--;
+  });
+
+  next();
 });
 
-app.use((req,res,next)=>{
-  onlineUsers++;
-  res.on("finish", ()=>{ onlineUsers--; });
-  next();
+// Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Get VPN codes
+app.get("/api/codes", (req, res) => {
+  try {
+    const data = fs.readFileSync(codesFile, "utf8");
+    res.json(JSON.parse(data));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Unable to read codes.json"
+    });
+  }
+});
+
+// Update VPN codes
+app.post("/api/update", (req, res) => {
+  try {
+    fs.writeFileSync(
+      codesFile,
+      JSON.stringify(req.body, null, 2),
+      "utf8"
+    );
+
+    res.json({
+      success: true,
+      message: "Updated successfully"
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      error: "Unable to update codes.json"
+    });
+  }
+});
+
+// Online users
+app.get("/api/users", (req, res) => {
+  res.json({
+    users: onlineUsers
+  });
+});
+
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`SRVpn Server Running on port ${PORT}`);
 });
